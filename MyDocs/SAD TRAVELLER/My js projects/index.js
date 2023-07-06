04c525e5510163765f28a73b2847d810294b932b
Farous Arthur <arthurfarous6@gmail.com>
2023-07-06 05:14:46 +0000

    addition

---
+++ MyDocs/SAD TRAVELLER/My js projects/index.js
@@ -0,0 +1,17 @@
+ 
+    
+    let a = prompt("Enter your weight ");
+function convertKgTolb() {
+    let  result = parseInt(a) * 2.2;
+    return result;
+
+ }
+
+ let output =convertKgTolb().toFixed(2);
+
+ alert("Your new weight is" + " " + output + "" + "lb");
+
+
+    
+
+
